50cd754c6d2342cb78b82068a305dc8f7ea37c6c
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-21 20:50:49 +0500

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+// creating user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: 'string',
+        required: true,
+    }
+}, {
+    timestamps: true,
+});
+
+const User = new mongoose.model('User', userSchema);
+
+export default User;
