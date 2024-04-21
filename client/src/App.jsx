bbd66a4e06a7e99835ae3488f9458f47557da61b
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-21 10:37:38 +0500

    added routed and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import SignIn from "../src/pages/SignIn";
+import SignUp from "../src/pages/SignUp";
+import Profile from "../src/pages/Profile";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
